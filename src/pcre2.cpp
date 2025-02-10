fb14551b1128f04126c99b8d4296b5641ac82db8
Segev Finer <segev208@gmail.com>
2025-02-10 17:21:49 +0200

    Add test method

--- src/pcre2.cpp
+++ src/pcre2.cpp
@@ -12,13 +12,15 @@ public:
     PCRE2& operator=(const PCRE2&) = delete;
 
 private:
+    Napi::Value Test(const Napi::CallbackInfo &info);
+
     pcre2_code *m_re;
     pcre2_match_data *m_match_data;
 };
 
 Napi::Object PCRE2::Init(Napi::Env env, Napi::Object exports) {
     Napi::Function func = DefineClass(env, "PCRE2", {
-
+        InstanceMethod<&PCRE2::Test>("test"),
     });
 
     exports.Set("PCRE2", func);
@@ -37,12 +39,13 @@ PCRE2::PCRE2(const Napi::CallbackInfo &info)
     }
 
     Napi::String pattern = info[0].As<Napi::String>();
+    std::string patternStr = pattern.Utf8Value();
 
     int errornumber;
     size_t erroroffset;
     m_re = pcre2_compile(
-        reinterpret_cast<PCRE2_SPTR>(pattern.Utf8Value().c_str()),
-        pattern.Utf8Value().size(),
+        reinterpret_cast<PCRE2_SPTR>(patternStr.c_str()),
+        patternStr.size(),
         PCRE2_UTF,
         &errornumber,
         &erroroffset,
@@ -69,6 +72,40 @@ PCRE2::PCRE2(const Napi::CallbackInfo &info)
 PCRE2::~PCRE2() {
     pcre2_match_data_free(m_match_data);
     pcre2_code_free(m_re);
+}
+
+Napi::Value PCRE2::Test(const Napi::CallbackInfo &info)
+{
+    if (info.Length() < 1) {
+        throw Napi::TypeError::New(info.Env(), "Wrong number of arguments");
+    }
+    if (!info[0].IsString()) {
+        throw Napi::TypeError::New(info.Env(), "Expected a string");
+    }
+
+    Napi::String subject = info[0].As<Napi::String>();
+    std::string subjectStr = subject.Utf8Value();
+
+    int rc = pcre2_match(
+        m_re,
+        reinterpret_cast<PCRE2_SPTR>(subjectStr.c_str()),
+        subjectStr.length(),
+        0,
+        0,
+        m_match_data,
+        nullptr
+    );
+    if (rc < 0) {
+        if (rc == PCRE2_ERROR_NOMATCH) {
+            return Napi::Boolean::New(info.Env(), false);
+        } else {
+            std::ostringstream oss;
+            oss << "PCRE2 matching error:" << rc;
+            throw Napi::Error::New(info.Env(), oss.str());
+        }
+    }
+
+    return Napi::Boolean::New(info.Env(), true);
 }
 
 static Napi::Object Init(Napi::Env env, Napi::Object exports) {
